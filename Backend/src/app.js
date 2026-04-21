dd12cce700999eba45e8cb1288d76c206db3f1d4
Md-musavvir <musavvirahmed490@gmail.com>
2026-04-21 13:53:35 +0530

    refactor: Update CORS configuration to allow all origins dynamically

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,25 +6,18 @@ import adminRouter from "./routes/admin.routes.js";
 import userRouter from "./routes/user.routes.js";
 
 const app = express();
-
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://major-project-beige-beta.vercel.app",
-    ],
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
     credentials: true,
   }),
 );
-
-app.options("*", cors());
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/user", userRouter);
-
 export default app;
