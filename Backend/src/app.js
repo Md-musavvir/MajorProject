780b30f9daaf7974e756ef777bfaa46849057ccb
Md-musavvir <musavvirahmed490@gmail.com>
2026-04-21 11:37:32 +0530

    refactor: Simplify CORS configuration by using a static origin array

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,18 +6,25 @@ import adminRouter from "./routes/admin.routes.js";
 import userRouter from "./routes/user.routes.js";
 
 const app = express();
+
 app.use(
   cors({
-    origin: (origin, callback) => {
-      callback(null, true);
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://major-project-beige-beta.vercel.app",
+    ],
     credentials: true,
-  })
+  }),
 );
+
+app.options("*", cors());
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/user", userRouter);
+
 export default app;
