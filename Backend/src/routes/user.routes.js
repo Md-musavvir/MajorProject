eda956daeb506d505fde765244061fbb78a220c1
Md-musavvir <musavvirahmed490@gmail.com>
2026-09-21 18:14:34 +0530

    refactor: Standardize import quotes and remove duplicate route definitions in user routes

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 import {
   addToCart,
@@ -14,15 +14,17 @@ import {
   returnOrders,
   returnSearch,
   returnUserData,
-} from "../controllers/user.controllers.js";
-import verifyJwt from "../middlewares/verifyJwt.js";
+} from '../controllers/user.controllers.js';
+import verifyJwt from '../middlewares/verifyJwt.js';
 
 const router = Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/getBook", returnSearch);
+router.get("/fetchBook/:bookId", returnBookById);
+router.get("/getBooks/:category", returnByCategory);
 router.use(verifyJwt);
 router.post("/logout", logoutUser);
-router.get("/getBooks/:category", returnByCategory);
 router.post("/addToCart", addToCart);
 router.put("/removeFromCart", removeFromCart);
 router.get("/getCart", ReturnCart);
@@ -30,6 +32,5 @@ router.put("/changePassword", changePassword);
 router.post("/placeOrder", placeOrder);
 router.get("/userData", returnUserData);
 router.get("/getOrders", returnOrders);
-router.get("/getBook", returnSearch);
-router.get("/fetchBook/:bookId", returnBookById);
+
 export default router;
